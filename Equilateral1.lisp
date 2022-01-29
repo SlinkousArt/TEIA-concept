941090e3d8d0f77e700ec1bde53d7bbad9428aac
Spencer Linkous <83504509+SlinkousArt@users.noreply.github.com>
2022-01-29 13:17:16 -0500

    Lorge
    made it thicc

--- Equilateral1.lisp
+++ Equilateral1.lisp
@@ -1,18 +1,18 @@
 ; Ronin v2.50
 
-(def T "M0,0 L100,0 M50,0 L50,100")
-(def E "M0,0 L100,0 M0,100 L100,100")
-(def I "M0,0 L0,100")
-(def A1 "M0,100 L50,0 M50,0 L100,100 M100,100 L0,100")
-(def A2 "M0,100 L57.75,0 M57.75,0 L115.5,100 M115.5,100 L0,100")
+(def T "M0,0 L200,0 M100,0 L100,200")
+(def E "M0,0 L200,0 M0,200 L200,200")
+(def I "M0,0 L0,200")
+(def A1 "M0,200 L100,0 M100,0 L200,200 M200,200 L0,200")
+(def A2 "M0,200 L115.5,0 M115.5,0 L231,200 M231,200 L0,200")
 
 (clear)
-(resize 505.5 140)
+(resize 1011 280)
 (stroke 
-  (svg 20 20 T) "white" 5)
+  (svg 40 40 T) "white" 10)
 (stroke 
-  (svg 170 20 E) "white" 5)
+  (svg 340 40 E) "white" 10)
 (stroke 
-  (svg 320 20 I) "white" 5)
+  (svg 640 40 I) "white" 10)
 (stroke 
-  (svg 370 20 A2) "white" 5)
+  (svg 740 40 A2) "white" 10)
